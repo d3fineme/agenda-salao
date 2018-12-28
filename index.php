e1e490f89ef1f6574e1f564ffef4a0a2b206b730
d3fineme <felipe-alves-09@hotmail.com>
2018-12-27 23:28:19 -0200

    login
    adicionado o login

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-<?php
-    include 'header.php';
-?>
-    <!-- Main Menu area End-->
-    <div class="container">
-        <center><img src="img/body.png" /></center>
-    </div>
-<?php
-    include 'footer.php';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Centro Estética Maria Bonita</title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<link rel="stylesheet" href="assets/css/login.css" />
+</head>
+<body>
+	<div class="jumbotron">
+	  <div class="container">
+	    <span class="glyphicon glyphicon-list-alt"></span>
+	    <h2>Agenda</h2>
+	    <div class="box">
+	        <input type="text" placeholder="username">
+		    <input type="password" placeholder="password">
+		    <a href="calendar/index.php"><button class="btn btn-default full-width"><span class="glyphicon glyphicon-ok"></span></button></a>
+	    </div>
+	  </div>
+	</div>
+</body>
+</html>
